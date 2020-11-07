1a618d7938e875349d431de34d2fd9bd588f2d14
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-11-07 18:57:16 -0300

    - Atualização de README.md - Melhorias nos comentarios do Código

--- README.md
+++ README.md
@@ -1,17 +1,32 @@
 # BMOBasicFunctionBasedWSGI
 BMOBasicFunctionBasedWSGI: Programa para executar funções basicas baseadas em WSGI na linguagem Python
 
+--------------------
+
 ## Desenvolvimento para a disciplina de Segurança de Rede de Computadores da UPE-Poli
 
 A técnica se dá com a construção da Interface de Gateway de Servidor Web (WSGI) simples baseada em uma função do Python. 
  
 
+--------------------
+
+
+## Como utilizar o programa:
+
+1) Execute o arquivo "app.py" com Python 3.7
+2) Abra no seu navegador o arquivo "CliqueAquiParaProgramaGET.html" ou "CliqueAquiParaProgramaPOST.html"
+3) Preencha seus dados nos campos "Usuario" e "E-mail"
+4) Clique no botão "Fazer requisição" para enviar o formulario que será interpretado via CGi. Isso fará uma pagina ser exibida/criada com os dados que você digitou.
+5) [FIM]
 
 
 --------------------
 
 
 
-Video tutorial: https://www.youtube.com/watch?v=O1Bm9zYhWQk
+Video tutorial de Inpiração:https://www.youtube.com/watch?v=O1Bm9zYhWQk
 
+Artigo POST de Inpiração:   http://wsgi.tutorial.codepoint.net/parsing-the-request-post
+
+Artigo GET de Inpiração:    http://wsgi.tutorial.codepoint.net/parsing-the-request-get
 
--- rede/app.py
+++ rede/app.py
@@ -2,39 +2,40 @@
 from wsgiref.simple_server import make_server
 from cgi import parse_qs, escape
 
-
+#Remoção de Warnings do console
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 def web_app(environment, response):
+    #Criação de cabeçalho da resposta informando que houve um recebimento OK
     status = '200 OK'
     headers = [('Content-type', 'text/html; charset=utf-8')]
     response(status, headers)
 
-    #form = cgi.FieldStorage()
-    # the environment variable CONTENT_LENGTH may be empty(if its GET) or not(if its POST)
+    # A variável de environment(ambiente) CONTENT_LENGTH pode estar vazia(se for GET) ou não(se for POST)
     try:
         request_body_size = int(environment.get('CONTENT_LENGTH', 0))
     except (ValueError):
         request_body_size = 0
 
-    # When the method is POST the variable will be sent
-    # in the HTTP request body which is passed by the WSGI server
-    # in the file like wsgi.input environment variable.
+    # Quando o método for POST a variável será enviada
+    # no corpo da solicitação HTTP que é passado pelo servidor WSGI
+    # no próprio arquivo como sendo a variável de ambiente wsgi.input
     if request_body_size > 0:       #Se for POST:
         d = parse_qs(environment['wsgi.input'].read(request_body_size))
-        usuario = (d.get(b'usuario', [''])[0]).decode('utf-8')  # Returns the first usuario value.
-        enderecoDeEmail = (d.get(b'enderecoDeEmail', [''])[0]).decode('utf-8')  # Returns the first enderecoDeEmail value.
+        usuario = (d.get(b'usuario', [''])[0]).decode('utf-8')
+        enderecoDeEmail = (d.get(b'enderecoDeEmail', [''])[0]).decode('utf-8')
     else:                           #Se for GET:
         d = parse_qs(environment['QUERY_STRING'])
-        usuario = d.get('usuario', [''])[0]  # Returns the first usuario value.
-        enderecoDeEmail = d.get('enderecoDeEmail', [''])[0]  # Returns the first enderecoDeEmail value.
+        usuario = d.get('usuario', [''])[0]
+        enderecoDeEmail = d.get('enderecoDeEmail', [''])[0]
 
-    # Always escape user input to avoid script injection
+    # Sempre escape(livre-se) de novas entradas/digitações do usuário para evitar injeção de script
     usuario = escape(usuario)
     enderecoDeEmail = escape(enderecoDeEmail)
 
+    # Cria o corpo da resposta HTTP para ser exibida no navegador do usuario
     resposta = ("<strong>Seu usuário é: " + usuario + "</strong>"
                 "<br />"
                 "<strong>Seu e-mail é: " + enderecoDeEmail + "</strong>")
@@ -45,6 +46,7 @@ def web_app(environment, response):
 
 with make_server('', 8000, web_app) as server:
     print("Servindo na porta 8000...\nVisite http://127.0.0.1:8000\nPara finalizar o servidor insira 'Control + C' ")
+    # Deixa o servidor ligado para sempre ou até que seu administrador encerre sua execução
     server.serve_forever()
 
 
