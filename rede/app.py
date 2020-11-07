4a44a92bb8a140c2f72a70e9bcd5611adcc313ab
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-11-07 18:37:36 -0300

    - Tudo funcionando para requisição GET - PROJETO CONCLUIDO

--- rede/app.py
+++ rede/app.py
@@ -12,11 +12,8 @@ def web_app(environment, response):
     headers = [('Content-type', 'text/html; charset=utf-8')]
     response(status, headers)
 
-
-
-
     #form = cgi.FieldStorage()
-    # the environment variable CONTENT_LENGTH may be empty or missing
+    # the environment variable CONTENT_LENGTH may be empty(if its GET) or not(if its POST)
     try:
         request_body_size = int(environment.get('CONTENT_LENGTH', 0))
     except (ValueError):
@@ -25,16 +22,18 @@ def web_app(environment, response):
     # When the method is POST the variable will be sent
     # in the HTTP request body which is passed by the WSGI server
     # in the file like wsgi.input environment variable.
-    request_body = environment['wsgi.input'].read(request_body_size)
-    d = parse_qs(request_body)
-
-    usuario = (d.get(b'usuario', [''])[0]).decode('utf-8')  # Returns the first usuario value.
-    enderecoDeEmail = (d.get(b'enderecoDeEmail', [''])[0]).decode('utf-8')  # Returns the first enderecoDeEmail value.
+    if request_body_size > 0:       #Se for POST:
+        d = parse_qs(environment['wsgi.input'].read(request_body_size))
+        usuario = (d.get(b'usuario', [''])[0]).decode('utf-8')  # Returns the first usuario value.
+        enderecoDeEmail = (d.get(b'enderecoDeEmail', [''])[0]).decode('utf-8')  # Returns the first enderecoDeEmail value.
+    else:                           #Se for GET:
+        d = parse_qs(environment['QUERY_STRING'])
+        usuario = d.get('usuario', [''])[0]  # Returns the first usuario value.
+        enderecoDeEmail = d.get('enderecoDeEmail', [''])[0]  # Returns the first enderecoDeEmail value.
 
     # Always escape user input to avoid script injection
     usuario = escape(usuario)
     enderecoDeEmail = escape(enderecoDeEmail)
-
 
     resposta = ("<strong>Seu usuário é: " + usuario + "</strong>"
                 "<br />"
