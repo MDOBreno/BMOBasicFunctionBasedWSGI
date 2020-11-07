c67467a83afb6d0a222035c4f4fc2eacf400c6c2
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-11-07 13:44:11 -0300

    - Adição de bibliotecas CGi - Ajustes para poder criar o corpo da pagina HTML

--- rede/app.py
+++ rede/app.py
@@ -1,14 +1,27 @@
+
 from wsgiref.simple_server import make_server
+import cgi, cgitb
+
+
 
 def web_app(environment, response):
     status = '200 OK'
     headers = [('Content-type', 'text/html; charset=utf-8')]
     response(status, headers)
 
-    return [b'<strong>Ola Mundo Eu acabei de criar meu primeiro WSGI</strong>']
+    form = cgi.FieldStorage()
+    usuario = "Ola" #form["usuario"].value
+    enderecoDeEmail = "Mundo" #form["enderecoDeEmail"].value
+    resposta = ("<strong>Seu usuário é: " + usuario + "</strong>"
+                "<br />"
+                "<strong>Seu e-mail é: " + enderecoDeEmail + "</strong>")
+
+    return [resposta.encode()]
+
 
 
 with make_server('', 8000, web_app) as server:
     print("Servindo na porta 8000...\nVisite http://127.0.0.1:8000\nPara finalizar o servidor insira 'Control + C' ")
-
     server.serve_forever()
+
+
