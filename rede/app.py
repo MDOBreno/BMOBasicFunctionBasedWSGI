0bea7109434e0e19e62f0a3cebdfb0ddfb95ebe5
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-11-07 18:07:36 -0300

    - Tudo funcionando para requisição POST

--- rede/app.py
+++ rede/app.py
@@ -1,7 +1,10 @@
 
 from wsgiref.simple_server import make_server
-import cgi, cgitb
+from cgi import parse_qs, escape
 
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 def web_app(environment, response):
@@ -9,12 +12,33 @@ def web_app(environment, response):
     headers = [('Content-type', 'text/html; charset=utf-8')]
     response(status, headers)
 
-    form = cgi.FieldStorage()
-    usuario = "Ola" #form["usuario"].value
-    enderecoDeEmail = "Mundo" #form["enderecoDeEmail"].value
-    resposta = ("<strong>Seu usuario e: " + usuario + "</strong>"
+
+
+
+    #form = cgi.FieldStorage()
+    # the environment variable CONTENT_LENGTH may be empty or missing
+    try:
+        request_body_size = int(environment.get('CONTENT_LENGTH', 0))
+    except (ValueError):
+        request_body_size = 0
+
+    # When the method is POST the variable will be sent
+    # in the HTTP request body which is passed by the WSGI server
+    # in the file like wsgi.input environment variable.
+    request_body = environment['wsgi.input'].read(request_body_size)
+    d = parse_qs(request_body)
+
+    usuario = (d.get(b'usuario', [''])[0]).decode('utf-8')  # Returns the first usuario value.
+    enderecoDeEmail = (d.get(b'enderecoDeEmail', [''])[0]).decode('utf-8')  # Returns the first enderecoDeEmail value.
+
+    # Always escape user input to avoid script injection
+    usuario = escape(usuario)
+    enderecoDeEmail = escape(enderecoDeEmail)
+
+
+    resposta = ("<strong>Seu usuário é: " + usuario + "</strong>"
                 "<br />"
-                "<strong>Seu e-mail e: " + enderecoDeEmail + "</strong>")
+                "<strong>Seu e-mail é: " + enderecoDeEmail + "</strong>")
 
     return [resposta.encode()]
 
